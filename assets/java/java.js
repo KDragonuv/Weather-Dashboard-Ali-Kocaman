4b24b5c903471341af391cbb6a11f6c952b752cb
Ali K <aliekx609@gmail.com>
2023-11-09 23:53:49 +0000

    connecting to the api + minor details on html

--- assets/java/java.js
+++ assets/java/java.js
@@ -9,6 +9,27 @@
 //THEN I am again presented with current and future conditions for that city
 
 var apiKey = "b37982cfc20fc5db367db6fb514be765"
-var city = "london"
+var city = "London"
 var cityURL = "https://api.openweathermap.org/data/2.5/forecast?q=' +city+ '&appid=' +apiKey"
-fetch ('https://api.openweathermap.org/data/2.5/forecast?q=' +city+ '&appid=' +apiKey);
+fetch ('https://api.openweathermap.org/data/2.5/forecast?q=' +city+ '&appid=' +apiKey)
+
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data); 
+        
+        document.getElementById('city').textContent = data.city.name;
+        
+        document.getElementById('temperature').textContent = temp.tofixed(0);
+        
+        var kelvin = data.list[i].dt_txt;
+        var temp = kelvin - 273.15
+        unixValue = data.list[i].dt_txt;
+        formattedDate = dayjs(unixValue);
+        
+        document.getElementById('description').textContent = data.list[0].weather[0].description
+
+
+  });
+       
